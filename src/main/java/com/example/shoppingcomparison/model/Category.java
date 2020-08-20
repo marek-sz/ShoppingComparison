9a95748f1630d242cecbc8deee00eba628152120
Marek Szymkowiak <marekszymkowiak1@gmail.com>
2020-08-20 23:32:56 +0200

    fixed controller, it responses on passed category parameter now

---
+++ src/main/java/com/example/shoppingcomparison/model/Category.java
@@ -0,0 +1,20 @@
+package com.example.shoppingcomparison.model;
+
+public enum Category {
+
+    ACCESORIE,
+    UNDERWEAR,
+    BLOUSE,
+    SHOE,
+    JEANS,
+    DUNGAREE,
+    SHIRT,
+    JACKET,
+    COAT,
+    LEGGINGS,
+    DRESS_JACKET,
+    SHORTS,
+    TROUSERS,
+    PURSE
+
+}
